325b7637bc9cd089a82389ffa0d84b828f2f1ff7
Jonathan M Borges <jonathan.m.borges@gmail.com>
2020-07-31 22:29:35 -0300

    Implementação do desafio sobre ReactJS - noções básicas do BootCampo GoStack da Reactseat.

---
+++ README.md
@@ -0,0 +1,23 @@
+## Sobre
+
+Este repositório foi criado como parte do curso [Bootcampo GoStack](https://rocketseat.com.br/gostack) da [Rocketseat](https://rocketseat.com.br).
+
+É a implementação de uma aplicação simples para demostrar o uso do ReactJS.
+
+Permite a criação, listagem, atualização e remoção de repositórios além disso permite que os repositórios possam receber "likes".
+
+## Referências
+
+- [Desafio 03: Conceitos do ReactJS](https://github.com/Rocketseat/bootcamp-gostack-desafios/tree/master/desafio-conceitos-reactjs)
+
+
+## Funcionalidades da aplicação
+
+- **`Listar os repositórios de uma API`**: Deve ser capaz de criar uma lista com o campo **title** de todos os repositórios que estão cadastrados na sua API.
+
+- **`Adicionar um repositório na API`**: Deve ser capaz de adicionar um novo item na sua API através de um botão com o texto **Adicionar** e, após a criação, deve ser capaz de exibir o nome dele após o cadastro.
+
+- **`Remover um repositório da API`**: Para cada item da sua lista, deve possuir um botão com o texto **Remover** que, ao clicar, irá chamar uma função para remover esse item da lista do seu frontend e da sua API.
+
+
+**Observação:** A API utilizada neste desafio está implementada em [Desafio Conceitos do NodeJS](https://github.com/jonathan-m-borges/desafio-conceitos-nodejs)
--- src/App.js
+++ src/App.js
@@ -1,28 +1,69 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./styles.css";
 
+import api from './services/api';
+
 function App() {
+  const [repositories, setRepositories] = useState([]);
+  const [url, setUrl] = useState('');
+  const [title, setTitle] = useState('');
+  const [techs, setTechs] = useState('');
+
+  useEffect(() => {
+    refreshRepositories();
+  }, []);
+
+  function refreshRepositories() {
+    api.get('/repositories')
+      .then(response => {
+        setRepositories(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+
   async function handleAddRepository() {
-    // TODO
+    const data = { url, title, techs: techs.split(/ *, */) };
+    const response = await api.post('/repositories', data);
+    setRepositories([...repositories, response.data]);
+    setUrl('');
+    setTitle('');
+    setTechs('');
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`/repositories/${id}`);
+    setRepositories(repositories.filter(x => x.id != id));
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
+        {repositories.map(repo => (
+          <li key={repo.id}>
+            {repo.title}
+            <button onClick={() => handleRemoveRepository(repo.id)}>
+              Remover
           </button>
-        </li>
+          </li>
+        ))}
+
       </ul>
 
+      <p>
+        <label htmlFor="url">URL:</label><br />
+        <input type="text" name="url" value={url} onChange={e => setUrl(e.target.value)}></input>
+      </p>
+      <p>
+        <label htmlFor="title">Title:</label><br />
+        <input type="text" name="title" value={title} onChange={e => setTitle(e.target.value)}></input>
+      </p>
+      <p>
+        <label htmlFor="techs">Techs:</label><br />
+        <input type="text" name="techs" value={techs} onChange={e => setTechs(e.target.value)}></input>
+      </p>
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
